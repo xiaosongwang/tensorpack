ed4c1118fdea58d1ca3064d1c15373abcb46fbc9
Xiaosong Wang <xiaosong.wang@live.com>
2017-05-08 14:42:16 -0400

    multilabel classification with end-to-end CNN+RNN

--- tensorpack/callbacks/inference.py
+++ tensorpack/callbacks/inference.py
@@ -9,11 +9,11 @@ import six
 from six.moves import zip
 
 from ..utils import logger
-from ..utils.stats import RatioCounter, BinaryStatistics
+from ..utils.stats import RatioCounter, BinaryStatistics, MultiLabelStatistics
 from ..tfutils import get_op_tensor_name
 
 __all__ = ['ScalarStats', 'Inferencer',
-           'ClassificationError', 'BinaryClassificationStats']
+           'ClassificationError', 'BinaryClassificationStats', 'MultiLabelClassificationStats']
 
 # TODO rename get_output_tensors to get_output_names
 
@@ -153,6 +153,52 @@ class ClassificationError(Inferencer):
     def _after_inference(self):
         return {self.summary_name: self.err_stat.ratio}
 
+class MultiLabelClassificationError(Inferencer):
+    """
+    Compute classification error in batch mode, from a ``wrong`` tensor.
+
+    The ``wrong`` tensor is supposed to be an binary vector containing
+    whether each sample in the batch is *incorrectly* classified.
+    You can use ``tf.nn.in_top_k`` to produce this vector.
+
+    This Inferencer produces the "true" error,
+    taking account of the fact that batches might not have the same size in
+    testing (because the size of test set might not be a multiple of batch size).
+    Therefore the result can be different from averaging the error rate of each batch.
+    """
+
+    def __init__(self, wrong_tensor_name='incorrect_vector', summary_name='val_error'):
+        """
+        Args:
+            wrong_tensor_name(str): name of the ``wrong`` tensor.
+                The default is the same as the default output name of
+                :meth:`prediction_incorrect`.
+            summary_name(str): the name to log the error with.
+        """
+        self.wrong_tensor_name = wrong_tensor_name
+        self.summary_name = summary_name
+
+    def _get_output_tensors(self):
+        return [self.wrong_tensor_name]
+
+    def _before_inference(self):
+        self.err_stat = RatioCounter()
+
+    def _datapoint(self, outputs):
+        vec = outputs[0]
+        if vec.ndim == 0:
+            logger.error("[DEPRECATED] use a 'wrong vector' for ClassificationError instead of nr_wrong. Exiting..")
+            sys.exit(1)
+        else:
+            # TODO put shape assertion into inference-runner
+            assert vec.ndim == 1, "{} is not a vector!".format(self.wrong_tensor_name)
+            batch_size = len(vec)
+            wrong = np.sum(vec)
+        self.err_stat.feed(wrong, batch_size)
+
+    def _after_inference(self):
+        return {self.summary_name: self.err_stat.ratio}
+
 
 class BinaryClassificationStats(Inferencer):
     """
@@ -183,3 +229,34 @@ class BinaryClassificationStats(Inferencer):
     def _after_inference(self):
         return {self.prefix + '_precision': self.stat.precision,
                 self.prefix + '_recall': self.stat.recall}
+
+class MultiLabelClassificationStats(Inferencer):
+    """
+    Compute precision / recall in binary classification, given the
+    prediction vector and the label vector.
+    """
+
+    def __init__(self, pred_tensor_name, label_tensor_name, prefix='val'):
+        """
+        Args:
+            pred_tensor_name(str): name of the 0/1 prediction tensor.
+            label_tensor_name(str): name of the 0/1 label tensor.
+        """
+        self.pred_tensor_name = pred_tensor_name
+        self.label_tensor_name = label_tensor_name
+        self.prefix = prefix
+
+    def _get_output_tensors(self):
+        return [self.pred_tensor_name, self.label_tensor_name]
+
+    def _before_inference(self):
+        self.stat = MultiLabelStatistics()
+
+    def _datapoint(self, outputs):
+        pred, label = outputs
+        self.stat.feed(pred, label)
+
+    def _after_inference(self):
+        return {self.prefix + '_precision': self.stat.precision,
+                self.prefix + '_recall': self.stat.recall,
+                self.prefix + '_specificity': self.stat.specificity}
